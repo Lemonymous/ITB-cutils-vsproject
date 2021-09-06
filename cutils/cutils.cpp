75e473663051005cc0230c3b38f07c9c9ce1a938
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-09-06 15:20:14 +0200

    Add ability to reload cutils to the same lua table
    Add `isModuleLoaded` - checks if the lua table the module should use already exists.
    
    If the table we are loading into already exists, the table will be cleared, and repopulated according to the options provided.
    
    Add debug logs listing load options when loading the module.

--- cutils/cutils.cpp
+++ cutils/cutils.cpp
@@ -10,6 +10,14 @@
 
 #define DLLEXPORT __declspec(dllexport)
 
+bool isModuleLoaded(lua_State* L, const char* name) {
+	lua_getglobal(L, name);
+	bool result = !lua_isnil(L, -1);
+	lua_pop(L, 1);
+
+	return result;
+}
+
 extern "C" DLLEXPORT int luaopen_inspect(lua_State* L) {
 	if (lua_gettop(L) > 0) {
 		luaL_checktype(L, 1, LUA_TTABLE);
@@ -22,7 +30,30 @@ extern "C" DLLEXPORT int luaopen_inspect(lua_State* L) {
 		lua_pop(L, lua_gettop(L));
 	}
 
-	lua_newtable(L);
+	if (!isModuleLoaded(L, NAME)){
+		lua_newtable(L);
+		logd(L, "loading cutils...");
+	}
+	else {
+		lua_getglobal(L, NAME);
+		lua_pushnil(L);
+		while (lua_next(L, -2) != 0) {
+			lua_pop(L, 1);
+			lua_pushvalue(L, -1);
+			lua_pushnil(L);
+			lua_rawset(L, -4);
+		}
+
+		logd(L, "Reloading cutils...");
+	}
+
+	logd(L, "cutils options:");
+	logdf(L, "  name = %s", NAME);
+	logdf(L, "  debug = %s", DEBUG_MODE);
+	logdf(L, "  hex = %s", HEX);
+	logdf(L, "  verbose = %s", VERBOSE);
+	logdf(L, "  get = %s", GET_FUNCS);
+	logdf(L, "  set = %s", SET_FUNCS);
 
 	if (DEBUG_MODE) {
 		lua_pushstring(L, "Debug");
